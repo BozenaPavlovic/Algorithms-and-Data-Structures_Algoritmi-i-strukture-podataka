9c1c7e0fbe5c146f7d15b7a85d443cce23531d4f
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-05-21 17:04:04 +0200

    Implement traversal print tests for BST
    Added test cases to print the results of different traversal types.

--- Traversal/Bez parametra result (ispis umjesto vraćanja liste).py
+++ Traversal/Bez parametra result (ispis umjesto vraćanja liste).py
@@ -14,3 +14,17 @@ def traverse(self, node=None, traversaltype='in'):
         if node.left: self.traverse(node.left, traversaltype)
         if node.right: self.traverse(node.right, traversaltype)
         print(node.key, end=' ')
+
+
+
+# TEST
+bst = BST()
+for broj in [50, 30, 70, 20, 40, 60, 80]:
+    bst.insert(broj)
+
+print("In-order: ", end='')
+bst.traverse_print(traversal_type='in')     # Ispis: 20 30 40 50 60 70 80
+print("\nPre-order: ", end='')
+bst.traverse_print(traversal_type='pre')    # Ispis: 50 30 20 40 70 60 80
+print("\nPost-order: ", end='')
+bst.traverse_print(traversal_type='post')   # Ispis: 20 40 30 60 80 70 50
