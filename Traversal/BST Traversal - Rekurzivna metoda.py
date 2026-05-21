9ecd2b44e3228e5e9859cba63dd775173b074c3c
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-05-21 17:06:56 +0200

    Update BST Traversal - Rekurzivna metoda.py

--- Traversal/BST Traversal - Rekurzivna metoda.py
+++ Traversal/BST Traversal - Rekurzivna metoda.py
@@ -42,3 +42,12 @@ if __name__ == "__main__":
     print("In-order (uzlazno):", bst.traverse(traversal_type='in'))     # [20, 30, 40, 50, 60, 70, 80]
     print("Pre-order:", bst.traverse(traversal_type='pre'))             # [50, 30, 20, 40, 70, 60, 80]
     print("Post-order:", bst.traverse(traversal_type='post'))           # [20, 40, 30, 60, 80, 70, 50]
+
+
+
+# Napišite rekurzivnu metodu traverse koja će zadano binarno stablo ispisati in-order, 
+# pre-order, ili post-order postupkom, ovisno o ulaznom argumentu traversal_type, koji može 
+# imati vrijednosti 'in', 'pre' ili 'post'. Pretpostavite da funkcija nikad neće primiti preko 
+# argumenta traversal_type neku drugu vrijednost osim ove 3 navedene. Metoda treba imati 
+# prototip: 
+# def traverse(self, node=None, result=None, traversal_type='in'): 
