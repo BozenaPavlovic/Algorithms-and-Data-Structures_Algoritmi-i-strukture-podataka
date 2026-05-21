44052003ec3eb3de9d03d9ce3b8d12e4cf51c630
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-05-21 17:32:32 +0200

    Add function to count leaves in a tree

---
+++ Tree/count_leaves.py
@@ -0,0 +1,6 @@
+def count_leaves(node):
+    if not node:
+        return 0
+    if not node.left and not node.right:
+        return 1
+    return count_leaves(node.left) + count_leaves(node.right)
