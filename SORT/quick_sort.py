e94fc5e480fd2e931a84901f925107b391c879ff
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-05-28 17:59:38 +0200

    Create quick_sort.py

---
+++ SORT/quick_sort.py
@@ -0,0 +1,16 @@
+def quick_sort(arr, low, high):
+    if low < high:
+        pivot_idx = partition(arr, low, high)
+        quick_sort(arr, low, pivot_idx - 1)
+        quick_sort(arr, pivot_idx + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
