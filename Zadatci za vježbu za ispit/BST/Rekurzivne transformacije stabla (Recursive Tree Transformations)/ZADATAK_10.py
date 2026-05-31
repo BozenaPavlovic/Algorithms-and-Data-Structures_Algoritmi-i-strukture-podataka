189399adc62cb2ec3fbed29d43662669fccb4170
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-05-31 17:48:27 +0200

    Update ZADATAK_10.py

--- Zadatci za vježbu za ispit/BST/Rekurzivne transformacije stabla (Recursive Tree Transformations)/ZADATAK_10.py
+++ Zadatci za vježbu za ispit/BST/Rekurzivne transformacije stabla (Recursive Tree Transformations)/ZADATAK_10.py
@@ -1,15 +1,15 @@
 # U svakom čvoru zamijeniti vrijednost sa 1 ako je produkt potomaka paran, 0 ako je neparan
 
-def update_product_parity(node):
+def update_p_parity(node):
     if node is None:
         return 1  # neutralni element za množenje
     
     original_key = node.key
     
-    left_prod = update_product_parity(node.left)
-    right_prod = update_product_parity(node.right)
+    left_p = update_p_parity(node.left)
+    right_p = update_p_parity(node.right)
     
-    total_product = left_prod * right_prod
+    total_product = left_p * right_p
     
     node.key = 0 if total_product % 2 == 0 else 1
     
