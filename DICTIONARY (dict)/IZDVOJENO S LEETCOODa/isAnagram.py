786b2a65f2cf5c3c8dffb9e42061fc7caefa500f
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 20:27:20 +0200

    Update isAnagram.py

--- DICTIONARY (dict)/IZDVOJENO S LEETCOODa/isAnagram.py
+++ DICTIONARY (dict)/IZDVOJENO S LEETCOODa/isAnagram.py
@@ -7,27 +7,27 @@ class Solution(object):
             
         # 2. INICIJALIZACIJA BROJAČA
         # 'count_map' će čuvati informaciju: { SLOVO : KOLIKO_PUTA_SE_POJAVLJUJE }
-        count_map = {}
+        mapa = {}
         
         # 3. PUNJENJE RJEČNIKA (Prva riječ 's')
         for char in s:
-            if char in count_map:
+            if char in mapa:
                 # Ako je slovo već u tablici, samo povećaj brojač za 1
-                count_map[char] += 1
+                mapa[char] += 1
             else:
                 # Ako je slovo novo, postavi brojač na 1
-                count_map[char] = 1
+                mapa[char] = 1
                 
         # 4. PRAŽNJENJE RJEČNIKA I PROVJERA (Druga riječ 't')
         for char in t:
             # KLJUČNA PROVJERA:
             # Ako slova uopće nema u tablici ILI smo ga već potrošili (brojač je 0)
-            if char not in count_map or count_map[char] == 0:
+            if char not in mapa or mapa[char] == 0:
                 # To znači da 't' ima neko slovo viška ili slovo koje 's' nema
                 return False
                 
             # Ako je slovo tu, smanji mu broj dostupnih kopija za 1
-            count_map[char] -= 1
+            mapa[char] -= 1
             
         # 5. KRAJ
         # Ako smo prošli cijelu riječ 't' bez greške, znači da su slova identična
