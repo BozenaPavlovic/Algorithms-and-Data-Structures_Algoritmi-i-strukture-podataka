305a6fa1daffb809c9807d5812c11d8b6fbcae8c
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 11:09:14 +0200

    Update is_valid_bst.py

--- BST (binarno stablo)/IZDVOJENO S LEETCODE/is_valid_bst.py
+++ BST (binarno stablo)/IZDVOJENO S LEETCODE/is_valid_bst.py
@@ -1,17 +1,33 @@
 # https://leetcode.com/problems/validate-binary-search-tree/description/
 # 98. Validate Binary Search Tree
 
-def is_valid_bst(self, node=None, min_val=float('-inf'), max_val=float('inf')):
-    if node is None:
-        node = self.root
-    if node is None:
-        return True
 
-    # Trenutni čvor mora biti unutar [min_val, max_val]
-    if not (min_val < node.key < max_val):
-        return False
 
-    # Lijevo podstablo: sve vrijednosti moraju biti MANJE od node.key
-    # Desno podstablo: sve vrijednosti moraju biti VEĆE od node.key
-    return (self.is_valid_bst(node.left, min_val, node.key) and
-            self.is_valid_bst(node.right, node.key, max_val))
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        
+        # Pomoćna funkcija koja prati trenutni čvor i njegove granice
+        def validate(node, low, high):
+            # 1. Bazni slučaj: Prazno stablo je uvijek validan BST
+            if node is None:
+                return True
+            
+            # 2. Provjera krši li trenutni čvor granice
+            # Vrijednost mora biti strogo veća od low i strogo manja od high
+            if node.val <= low or node.val >= high:
+                return False
+            
+            # 3. Rekurzivni korak za lijevo i desno podstablo
+            # Za lijevo dijete: gornja granica postaje trenutni node.val
+            # Za desno dijete: donja granica postaje trenutni node.val
+            return validate(node.left, low, node.val) and validate(node.right, node.val, high)
+
+        # Na samom početku, korijen može imati bilo koju vrijednost
+        # Koristimo float('-inf') i float('inf') za minus i plus beskonačno
+        return validate(root, float('-inf'), float('inf'))
