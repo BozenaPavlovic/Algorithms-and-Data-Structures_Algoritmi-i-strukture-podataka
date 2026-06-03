d4c58587424d80383f0fb8e11b2cca407d8c2c95
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 23:10:21 +0200

    Update kth_smallest.py

--- BST (binarno stablo)/IZDVOJENO S LEETCODE/kth_smallest.py
+++ BST (binarno stablo)/IZDVOJENO S LEETCODE/kth_smallest.py
@@ -12,7 +12,7 @@ class Solution(object):
         list_num=[]
         def inorder(node):
             if node is None:
-                return 0
+                return None
             if node.left is not None:
                 inorder(node.left)
             list_num.append(node.val)
@@ -20,3 +20,29 @@ class Solution(object):
                 inorder(node.right)
         inorder(root)
         return list_num[k-1]
+
+
+
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        # 1. Prazna lista za spremanje sortiranih brojeva
+        list_num = []
+        
+        def inorder(node):
+            # BAZNI SLUČAJ: Ako si došao na prazno mjesto, samo se vrati natrag (bez 0!)
+            if node is None:
+                return
+            
+            # 2. IN-ORDER REDOSLIJED (Lijevo -> Trenutni -> Desno)
+            # Ne trebaju nam 'if' uvjeti za djecu, samo ih pozovemo.
+            # Ako je dijete None, funkcija će se pokrenuti i odmah stati na liniji 9.
+            inorder(node.left)          # Idi skroz lijevo
+            list_num.append(node.val)   # Spremi trenutni broj
+            inorder(node.right)         # Idi desno
+
+        # 3. Pokrenemo rekurziju od korijena
+        inorder(root)
+        
+        # 4. Vraćamo k-ti najmanji element (indeks k-1 jer liste kreću od 0)
+        return list_num[k - 1]
