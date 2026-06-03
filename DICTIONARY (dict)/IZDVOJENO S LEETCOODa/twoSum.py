032a3433cea2137e06553ca1ac0e3b43db39c791
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 17:39:53 +0200

    Update twoSum.py

--- DICTIONARY (dict)/IZDVOJENO S LEETCOODa/twoSum.py
+++ DICTIONARY (dict)/IZDVOJENO S LEETCOODa/twoSum.py
@@ -1,6 +1,30 @@
 
 class Solution(object):
     def twoSum(self, nums, target):
+        n = len(nums)
+        podsjetnik = {} # Prazna tablica za pamćenje brojeva i njihovih indeksa
+        
+        for i in range(n):
+            trenutni_broj = nums[i]
+            fali_mi = target - trenutni_broj
+            
+            # Umjesto druge for petlje, samo pitamo rječnik:
+            if fali_mi in podsjetnik:
+                return [podsjetnik[fali_mi], i] # Vraćamo indeks nađenog para i trenutni indeks i
+            
+            # Ako ga nema, zapišemo trenutni broj i njegov indeks u tablicu
+            podsjetnik[trenutni_broj] = i
+
+
+
+
+
+
+
+
+# brute FORCE
+class Solution(object):
+    def twoSum(self, nums, target):
         n = len ( nums)
         
         
