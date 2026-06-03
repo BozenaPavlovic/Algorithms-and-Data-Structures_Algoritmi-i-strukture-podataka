7332d7e8b8db756c20d2d350ee3f38e4379294cd
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 16:56:37 +0200

    Update kth_smallest.py

--- BST (binarno stablo)/IZDVOJENO S LEETCODE/kth_smallest.py
+++ BST (binarno stablo)/IZDVOJENO S LEETCODE/kth_smallest.py
@@ -1,36 +1,22 @@
 # https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
 # 230. Kth Smallest Element in a BST
 
-def kth_smallest(self, k, node=None, counter=None):
-    if node is None:
-        node = self.root
-    if counter is None:
-        counter = [0]  # koristimo listu da prođemo po referenci
-
-    # IN-ORDER obilazak (L → N → R)
-    result = self._inorder_kth(node, k, counter)
-    return result
-
-
-def _inorder_kth(self, node, k, counter):
-    if node is None:
-        return None
-
-    # Lijevo
-    left_result = self._inorder_kth(node.left, k, counter)
-    if left_result is not None:
-        return left_result
-
-    # Čvor (broji)
-    counter[0] += 1
-    if counter[0] == k:
-        return node.key
-
-    # Desno
-    return self._inorder_kth(node.right, k, counter)
-
-
-def kth_smallest_ALT(self, k):
-    # In-order obilazak vraća sortiranu listu
-    sorted_nodes = self.inorder()  # tvoja postojeca funkcija
-    return sorted_nodes[k-1] if k <= len(sorted_nodes) else None
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        list_num=[]
+        def inorder(node):
+            if node is None:
+                return 0
+            if node.left is not None:
+                inorder(node.left)
+            list_num.append(node.val)
+            if node.right is not None:
+                inorder(node.right)
+        inorder(root)
+        return list_num[k-1]
