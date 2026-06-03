083ab734334c61e9abbee341f866c13af3affd8f
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-03 12:35:21 +0200

    Update is_valid_bst.py

--- BST (binarno stablo)/IZDVOJENO S LEETCODE/is_valid_bst.py
+++ BST (binarno stablo)/IZDVOJENO S LEETCODE/is_valid_bst.py
@@ -16,16 +16,14 @@
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def is_valid(node, minn, maxx):
-            if not node:
+        def valid(node, minn, maxx):
+            if node is None:
                 return True
-            
             if node.val <= minn or node.val >= maxx:
                 return False
-            
-            return is_valid(node.left, minn, node.val) and is_valid(node.right, node.val, maxx)
+            return valid(node.left, minn, node.val) and valid(node.right, node.val, maxx)
 
-        return is_valid(root, float("-inf"), float("inf"))
+        return valid(root, float("-inf"), float("inf"))
 
 # Time Complexity: O(n)
 # Space Complexity: O(h) { here "h" is height of tree }
