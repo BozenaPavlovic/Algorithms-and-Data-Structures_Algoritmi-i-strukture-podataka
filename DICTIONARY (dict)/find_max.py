79982654c8fe821c98f08ba8348567d29cf0003b
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-06-08 00:51:58 +0200

    Update find_max.py

--- DICTIONARY (dict)/find_max.py
+++ DICTIONARY (dict)/find_max.py
@@ -21,3 +21,20 @@ find_max(test_dict)
 # Maksimalna vrijednost
 # Napišite funkciju `find_max` koja će pronaći maksimalnu vrijednost u riječniku, korištenjem `dict` metode `.items()`.
 # Trebat ćete koristi *for* petlju, ali u kojoj je iterator par ključ-vrijednost, jer metoda `.items()` vraća ključ-vrijednost parove.
+
+
+
+def find_min(input_dict):
+    mn = None
+    mn_key = None
+    
+    for key, value in input_dict.items():
+        if mn is None:
+            mn = value
+            mn_key = key
+        else:
+            if value < mn:  # Tražimo manju vrijednost
+                mn = value
+                mn_key = key
+
+    return mn_key, mn
