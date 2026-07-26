a2ad65c40d5c77306fe8b8994ec1229b36a7e220
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-07-26 20:53:22 +0200

    Update selection_sort.py

--- SORT/selection_sort.py
+++ SORT/selection_sort.py
@@ -8,3 +8,25 @@ def selection_sort(arr):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+# iz primjera s nastave prvi zadatak 
+#%%
+def find_min(arr):
+    mn = arr[0]
+    mn_idx = 0
+    k = 1
+    while k < len(arr):
+        if(mn > arr[k]):
+            mn = arr[k]
+            mn_idx = k
+        k += 1
+    return mn, mn_idx
+
+def selection_sort(arr):
+    for k in range(0,len(arr)-1):
+        temp = arr[k]
+        mn, mn_idx = find_min(arr[k:len(arr)])
+        arr[k] = mn
+        arr[k+mn_idx] = temp
+    return arr
