8eda45c2b48deab3a82c58ecadf37730bf638bb8
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-07-27 20:16:22 +0200

    Refactor selection sort for singly linked list

--- SORT/selection_sort_jednostruko_pov._lista.py
+++ SORT/selection_sort_jednostruko_pov._lista.py
@@ -1,16 +1,25 @@
-def selection_sort_singly(head):
-    i = head                            # for i in range(n):
-    while i:
-        min_node = i                    # min_idx = i
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def selection_sort(head):
+    if head is None or head.next is None:
+        return head
+    
+    start = head
+    while start is not None and start.next is not None:
+        min_node = start
+        current = start.next
         
-        j = i.next                      # for j in range(i + 1, n):
-        while j:
-            if j.data < min_node.data:  # if arr[j] < arr[min_idx]:
-                min_node = j            # min_idx = j
-            j = j.next
-            
-        # Profesorov trik: zamijeni samo podatke (arr[i], arr[min_idx] = ...)
-        i.data, min_node.data = min_node.data, i.data
-        i = i.next
+        while current is not None:
+            if current.data < min_node.data:
+                min_node = current
+            current = current.next
         
+        if min_node != start:
+            start.data, min_node.data = min_node.data, start.data
+        
+        start = start.next
+    
     return head
