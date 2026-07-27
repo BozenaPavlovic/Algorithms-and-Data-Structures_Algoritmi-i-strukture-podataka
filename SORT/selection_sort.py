a9e129e1255ecc0bc2bec834e270b2a6b509ebfc
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-07-27 20:14:18 +0200

    Implement selection sort in Lista class
    Added a class 'Lista' with a method to sort its data using selection sort.

--- SORT/selection_sort.py
+++ SORT/selection_sort.py
@@ -30,3 +30,39 @@ def selection_sort(arr):
         arr[k] = mn
         arr[k+mn_idx] = temp
     return arr
+
+
+
+
+
+# Ako zadatak kaže:
+# "Napiši funkciju koja prima listu i sortira je Selection Sortom"
+
+# Koristi arr:
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+# Ako zadatak kaže:
+# "Napiši klasu koja sadrži metodu za sortiranje svojih podataka"
+
+# Koristi self:
+class Lista:
+    def __init__(self, podaci):
+        self.podaci = podaci
+    
+    def selection_sort(self):
+        n = len(self.podaci)
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1, n):
+                if self.podaci[j] < self.podaci[min_idx]:
+                    min_idx = j
+            self.podaci[i], self.podaci[min_idx] = self.podaci[min_idx], self.podaci[i]
+        return self.podaci
