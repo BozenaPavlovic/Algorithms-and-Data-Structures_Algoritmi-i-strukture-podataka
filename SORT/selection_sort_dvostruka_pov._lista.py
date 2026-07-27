a13c52cab9e0ba7a1bc28ab1012429412abb5edc
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-07-27 20:15:36 +0200

    Implement selection sort for doubly linked list
    Refactor selection sort for doubly linked list with a new Node class.

--- SORT/selection_sort_dvostruka_pov._lista.py
+++ SORT/selection_sort_dvostruka_pov._lista.py
@@ -1,16 +1,26 @@
-def selection_sort_doubly(head):
-    i = head
-    while i:
-        min_node = i
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+def selection_sort(head):
+    if head is None or head.next is None:
+        return head
+    
+    start = head
+    while start is not None and start.next is not None:
+        min_node = start
+        current = start.next
         
-        j = i.next
-        while j:
-            if j.data < min_node.data:
-                min_node = j
-            j = j.next
-            
-        # Zamjena podataka unutar čvorova
-        i.data, min_node.data = min_node.data, i.data
-        i = i.next
+        while current is not None:
+            if current.data < min_node.data:
+                min_node = current
+            current = current.next
         
+        if min_node != start:
+            start.data, min_node.data = min_node.data, start.data
+        
+        start = start.next
+    
     return head
