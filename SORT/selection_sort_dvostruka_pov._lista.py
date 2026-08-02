dcd7a9938d21a5c0e2fd0b79ebd45a985a037cf8
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-08-02 14:08:32 +0200

    Add selection sort for doubly linked list
    Implemented selection sort for a doubly linked list, including handling of previous and next pointers.

--- SORT/selection_sort_dvostruka_pov._lista.py
+++ SORT/selection_sort_dvostruka_pov._lista.py
@@ -24,3 +24,67 @@ def selection_sort(head):
         start = start.next
     
     return head
+
+
+
+
+
+# DOUBLY LINKED LIST
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+def selection_sort(head):
+    if head is None or head.next is None:
+        return head
+
+    tail = None
+    current = head
+
+    while current is not None:
+        small = current
+        prev_small = None
+        prev = current
+        temp = current.next
+
+        # Traži minimum (isti kod kao za jednostruku)
+        while temp is not None:
+            if temp.data < small.data:
+                small = temp
+                prev_small = prev
+            prev = temp
+            temp = temp.next
+
+        # Izbaci minimum (isti kod)
+        if small == current:
+            current = current.next
+        else:
+            prev_small.next = small.next
+            if small.next is not None:
+                small.next.prev = prev_small   # 🟢 DODATAK za dvostruku
+
+        # Dodaj minimum na kraj
+        if tail is None:
+            head = small
+            small.prev = None                  # 🟢 DODATAK za dvostruku
+        else:
+            tail.next = small
+            small.prev = tail                  # 🟢 DODATAK za dvostruku
+
+        tail = small
+
+    return head
+
+
+
+
+
+
+
+
+
+
+
+
