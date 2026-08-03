175cd3bb4d6d06627552d267749e956f1270782f
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-08-03 10:57:43 +0200

    Update bucket_sort_jed_pov_l.py

--- SORT/bucket_sort_jed_pov_l.py
+++ SORT/bucket_sort_jed_pov_l.py
@@ -1,154 +1,68 @@
-# DATA SAWP
-
-# ==============================================================================
-# BUCKET SORT - JEDNOSTRUKO POVEZANA LISTA
-# ==============================================================================
+# Bubble sort za jednostruko povezanu listu (data-swap verzija)
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-
 class LinkedList:
     def __init__(self):
         self.head = None
 
+    def append(self, value):
+        """Dodaj vrijednost na kraj liste."""
+        node = Node(value)
+        if not self.head:
+            self.head = node
+            return
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = node
 
-# ==============================================================================
-# POMOĆNA FUNKCIJA 1: INSERTION SORT
-# ==============================================================================
+    @classmethod
+    def from_list(cls, values):
+        ll = cls()
+        for v in values:
+            ll.append(v)
+        return ll
 
-def insertion_sort(head):
-    """Sortira povezanu listu Insertion Sortom (rastući poredak)."""
-    if head is None or head.next is None:
-        return head
-    
-    sorted_head = None
-    current = head
-    
-    while current is not None:
-        next_node = current.next
-        
-        # Umetanje na početak
-        if sorted_head is None or sorted_head.data >= current.data:
-            current.next = sorted_head
-            sorted_head = current
-        else:
-            # Traženje pozicije
-            temp = sorted_head
-            while temp.next is not None and temp.next.data < current.data:
-                temp = temp.next
-            current.next = temp.next
-            temp.next = current
-        
-        current = next_node
-    
-    return sorted_head
+    def to_list(self):
+        out = []
+        cur = self.head
+        while cur:
+            out.append(cur.data)
+            cur = cur.next
+        return out
 
+    def bubble_sort(self):
+        """
+        Bubble sort koji radi samo zamjenu polja `data` između čvorova.
+        Efikasnija varijanta: prekidamo ako u prolazu nema zamjena i
+        sužavamo opseg pomoću end markera.
+        Sortira rastuće.
+        """
+        if self.head is None or self.head.next is None:
+            return
 
-# ==============================================================================
-# POMOĆNA FUNKCIJA 2: PRONALAŽENJE MIN I MAX
-# ==============================================================================
+        end = None
+        while end != self.head:
+            swapped = False
+            cur = self.head
+            while cur.next != end:
+                if cur.data > cur.next.data:
+                    # zamijeni samo podatke (data swap)
+                    cur.data, cur.next.data = cur.next.data, cur.data
+                    swapped = True
+                cur = cur.next
+            end = cur  # posljednji element je sada na svom mjestu
+            if not swapped:
+                break
 
-def find_min_max(head):
-    """Pronalazi minimalnu i maksimalnu vrijednost u listi."""
-    if head is None:
-        return None, None
-    
-    min_val = head.data
-    max_val = head.data
-    current = head
-    
-    while current is not None:
-        if current.data < min_val:
-            min_val = current.data
-        if current.data > max_val:
-            max_val = current.data
-        current = current.next
-    
-    return min_val, max_val
-
-
-# ==============================================================================
-# POMOĆNA FUNKCIJA 3: SPOJANJE PRETINACA
-# ==============================================================================
-
-def concatenate_buckets(buckets, bucket_count):
-    """Spaja sve pretince u jednu listu."""
-    sorted_head = None
-    sorted_tail = None
-    
-    for i in range(bucket_count):
-        if buckets[i] is not None:
-            if sorted_head is None:
-                sorted_head = buckets[i]
-            else:
-                sorted_tail.next = buckets[i]
-            
-            sorted_tail = buckets[i]
-            while sorted_tail.next is not None:
-                sorted_tail = sorted_tail.next
-    
-    return sorted_head
-
-
-# ==============================================================================
-# GLAVNA FUNKCIJA: BUCKET SORT
-# ==============================================================================
-
-def bucket_sort(head, bucket_count):
-    """
-    Sortira povezanu listu Bucket Sort algoritmom (rastući poredak).
-    
-    Argumenti:
-        head - glava liste
-        bucket_count - broj pretinaca
-    
-    Vraća:
-        Glavu sortirane liste
-    """
-    if head is None or head.next is None:
-        return head
-    
-    # 1. Pronađi min i max
-    min_val, max_val = find_min_max(head)
-    
-    # Ako su svi elementi isti
-    if min_val == max_val:
-        return head
-    
-    # 2. Kreiraj pretince
-    buckets = [None] * bucket_count
-    
-    # 3. Raspodijeli čvorove u pretince
-    current = head
-    while current is not None:
-        next_node = current.next
-        
-        # Izračunaj indeks pretinca
-        bucket_idx = int((current.data - min_val) * (bucket_count - 1) / (max_val - min_val))
-        
-        # Umetni na početak pretinca
-        current.next = buckets[bucket_idx]
-        buckets[bucket_idx] = current
-        
-        current = next_node
-    
-    # 4. Sortiraj svaki pretinac
-    for i in range(bucket_count):
-        if buckets[i] is not None:
-            buckets[i] = insertion_sort(buckets[i])
-    
-    # 5. Spoji sve pretince
-    return concatenate_buckets(buckets, bucket_count)
-
-
-# ==============================================================================
-# METODA UNUTAR KLASE
-# ==============================================================================
-
-class LinkedList:
-    def bucket_sort(self, bucket_count):
-        """Sortira listu Bucket Sort algoritmom (rastući poredak)."""
-        self.head = bucket_sort(self.head, bucket_count)
+# Primjer upotrebe
+if __name__ == "__main__":
+    values = [3, 1, 4, 2, 5]
+    ll = LinkedList.from_list(values)
+    print("Prije:", ll.to_list())
+    ll.bubble_sort()
+    print("Poslije:", ll.to_list())
