e2f14b1cd1d0bb70d25df55e0204cbb938c8b2f9
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-08-08 10:58:10 +0200

    Implement word frequency, merge dicts, and max depth
    Added functions for word frequency counting, merging dictionaries, and finding maximum value in a dictionary. Included a function to calculate the maximum depth of a binary search tree.

--- Bilježnica/10_sve ostalo s nastave.py
+++ Bilježnica/10_sve ostalo s nastave.py
@@ -74,5 +74,89 @@ def hello_world_recursive(n):
         print("Hello, World!")
         hello_world_recursive(n - 1)
 
+#BROJANJE UČESTALOSTI RIJEČI
+#%%
+def word_frequency(text):
+    word_freq = {}
+    words = text.split()
 
+    for word in words:
+        if len(word)>0:
+            if word in word_freq:
+                word_freq[word] += 1
+            else:
+                word_freq[word] = 1
+
+    return word_freq
+
+def custom_word_frequency(text):
+    word_freq = CustomDict()
+    words = text.split()
+
+    for word in words:
+        if len(word)>0:
+            if word_freq.contains(word):
+                word_freq.set(word, word_freq.get(word) + 1)
+            else:
+                word_freq.set(word, 1)
+
+    return word_freq
+
+
+
+# SPAJANJE RIJEČNIKA 
+#%%
+def merge_dicts(dict1, dict2):
+    result = dict1.copy()
+    for key in dict2.keys():
+        if key in dict1.keys():
+            result[key] += dict2[key]
+        else:
+            result[key] = dict2[key]
+
+    return result
+
+# Test
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+
+merge_dicts(dict1, dict2)
+
+# MAKSIMALNA VRIJEDNOST
+#%%
+def find_max(input_dict):
+    mx = None
+    mx_key = None
+    for key, value in input_dict.items():
+        if mx is None:
+            mx = value
+            mx_key = key
+        else:
+            if value > mx:
+                mx = value
+                mx_key = key
+
+    return mx_key, mx
+
+test_dict = {'a': 1, 'b': 5, 'c': 7, 'd': 5}
+find_max(test_dict)
+
+
+#BST
+#%%
+def max_depth(node):
+    if not node:
+      return 0
+
+    return 1 + max(max_depth(node.left), max_depth(node.right))
+
+bst = BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+bst.insert(1)
+
+print("Dubina stabla je:", max_depth(bst.root))  # Očekivano: 4
 
