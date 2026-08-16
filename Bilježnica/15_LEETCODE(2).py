eaa835499844a7b34023865043f6befedc1bc9ec
Božena Pavlović <188483010+BozenaPavlovic@users.noreply.github.com>
2026-08-16 11:53:05 +0200

    Update 15_LEETCODE(2).py

--- Bilježnica/15_LEETCODE(2).py
+++ Bilježnica/15_LEETCODE(2).py
@@ -85,4 +85,51 @@ def deleteDuplicates(head):
         else:
             curr = curr.next
     return head
-        
+
+ 876. Middle of the Linked List— Easy
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        if head is None:
+            return None
+        fast = head
+        slow = head
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+21. Merge Two Sorted Lists — Easy
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1 is None:
+            curr.next = list2
+        else:
+            curr.next = list1
+        return dummy.next
